1964bd5b94b29f6efea83529fd75ccd1b56431ff
Bomhae <tlsgjs7764@gmail.com>
2019-04-13 14:49:54 +0900

    컴포넌트 분리와 map함수 사용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Customer from "./components/Customer";
 import './App.css';
+
+const customers = [
+{
+  id : 1,
+  image : 'https://placeimg.com/64/64/1',
+  name : '이신헌',
+  birthday : '950725',
+  gender : 'male',
+  job : '대학생'
+},
+{
+  id : 2,
+  image : 'https://placeimg.com/64/64/2',
+  name : '박주영',
+  birthday : '890712',
+  gender : 'male',
+  job : '대학생'
+},
+{
+  id : 3,
+  image : 'https://placeimg.com/64/64/3',
+  name : '이병훈',
+  birthday : '920125',
+  gender : 'male',
+  job : '대학생'
+}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>hello</h2>
+      <div>
+        {customers.map(c => {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+        }
       </div>
-    );
+      )
   }
 }
 
